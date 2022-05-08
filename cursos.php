65e6c404f7f1b8f87ad95de96b6eb67987a121ad
gabriel leão <gabidonn@gmail.com>
2022-05-08 18:51:07 -0300

    organizar index + add cursos

---
+++ cursos.php
@@ -0,0 +1,74 @@
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+        <!-- cada coluna -->
+        <div class="col">
+          <div class="card shadow-sm">
+            <img width="100%" height="125px"  src="./assets/escolas/curso1.jpeg">
+            <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="125" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
+
+            <div class="card-body">
+              <p class="card-title"><b>Nome do Curso 1</b></p>
+              <p class="card-text more">Quem é profissional de RH sabe que a entrevista em um processo seletivo merece atenção especial. Afinal, ela identificará o candidato certo para determinada vaga e, a escolha na abordagem, nesse momento, é essencial. Desta forma, é possível contar com vários tipos de testes para aplicar em entrevistas que tornam o processo de recrutamento e seleção mais dinâmico e eficiente. O tipo de entrevista que o profissional de RH deve aplicar em sua empresa variará de acordo com alguns fatores como, por exemplo, a cultura da empresa, ou seja, sua política, suas crenças, valores e hábitos. É preciso também definir o perfil comportamental ideal, de acordo com as necessidades do cargo. Por isso, com o objetivo de facilitar na escolha do melhor tipo de entrevista para a sua organização, apresentaremos neste conteúdo alguns tipos de entrevistas mais utilizados por recrutadores, suas características, e como e quando elas devem ser aplicadas. </p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">+ Informações</button>
+                  <!-- apenas quem tiver permissão (admin ou revisor) -->
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Editar</button>
+                </div>
+                <!-- distancia (IMPORTANTE) -->
+                <small class="text-muted">750m</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col">
+          <div class="card shadow-sm">
+          <img width="100%" height="125px" src="./assets/escolas/curso2.jpg">
+            <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="125" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
+
+            <div class="card-body">
+              <p class="card-title"><b>Nome do Curso 2</b></p>
+              <p class="card-text more">Almoxarifado é o local destinado à guarda e conservação de materiais, em recinto coberto ou não, adequado à sua natureza, tendo a função de destinar espaços onde permanecerá cada item aguardando a necessidade do seu uso, ficando sua localização, equipamentos e disposição interna acondicionados à política geral de estoques da empresa.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">+ Informações</button>
+                  <!-- apenas quem tiver permissão (admin ou revisor) -->
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Editar</button>
+                </div>
+                <!-- distancia (IMPORTANTE) -->
+                <small class="text-muted">1.5km</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col">
+          <div class="card shadow-sm">
+          <img width="100%" height="125px" src="./assets/escolas/curso3.png">
+            <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="125" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
+
+            <div class="card-body">
+              <p class="card-title"><b>Nome do Curso 3</b></p>
+              <p class="card-text more">Aquisição de competências para a realização de apresentações eficazes e motivadoras em contextos formativos ou empresariais.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">+ Informações</button>
+                  <!-- apenas quem tiver permissão (admin ou revisor) -->
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Editar</button>
+                </div>
+                <!-- distancia (IMPORTANTE) -->
+                <small class="text-muted">3.5km</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+</div>
+
+<!-- TO-DO: melhorar esse botão e add função -->
+<div class="centralizado">
+    <button type="button" label="Ver mais">Ver mais</button>
+</div>
--- index.php
+++ index.php
@@ -10,84 +10,20 @@
 
 <body>
     <div class="container">
-    <!-- dinamizar com PHP a partir daqui -->
 
-      <!-- div responsável por dividir os elementos em 3 colunas por linha -->
-      <h1>Escolas</h1>
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+      <h1>Instituições</h1>
+      <?php include_once __DIR__ . '/instituicoes.php'; ?>
 
-        <!-- cada coluna -->
-        <div class="col">
-          <div class="card shadow-sm">
-            <img width="100%" height="125px"  src="./assets/escolas/escola1.png">
-            <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="125" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
-
-            <div class="card-body">
-              <p class="card-title"><b>Nome do Colégio 1</b></p>
-              <p class="card-text more">Nosso compromisso é contribuir para o fortalecimento intelectual e emocional dos nossos alunos, tornando extremamente simples a comunicação deles com pessoas de todas as partes do mundo, através do ensino efetivo original e inovador, capaz de fazer do aprendizado uma experiência única e prazerosa.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">+ Informações</button>
-                  <!-- apenas quem tiver permissão (admin ou revisor) -->
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Editar</button>
-                </div>
-                <!-- distancia (IMPORTANTE) -->
-                <small class="text-muted">750m</small>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-        <div class="col">
-          <div class="card shadow-sm">
-          <img width="100%" height="125px" src="./assets/escolas/escola2.jpg">
-            <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="125" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
-
-            <div class="card-body">
-              <p class="card-title"><b>Nome do Colégio 2</b></p>
-              <p class="card-text more">Nosso compromisso é contribuir para o fortalecimento intelectual e emocional dos nossos alunos, tornando extremamente simples a comunicação deles com pessoas de todas as partes do mundo, através do ensino efetivo original e inovador, capaz de fazer do aprendizado uma experiência única e prazerosa.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">+ Informações</button>
-                  <!-- apenas quem tiver permissão (admin ou revisor) -->
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Editar</button>
-                </div>
-                <!-- distancia (IMPORTANTE) -->
-                <small class="text-muted">1.5km</small>
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-        <div class="col">
-          <div class="card shadow-sm">
-          <img width="100%" height="125px" src="./assets/escolas/escola3.jpg">
-            <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="125" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
-
-            <div class="card-body">
-              <p class="card-title"><b>Nome do Colégio 3</b></p>
-              <p class="card-text more">Nosso compromisso é contribuir para o fortalecimento intelectual e emocional dos nossos alunos, tornando extremamente simples a comunicação deles com pessoas de todas as partes do mundo, através do ensino efetivo original e inovador, capaz de fazer do aprendizado uma experiência única e prazerosa.</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">+ Informações</button>
-                  <!-- apenas quem tiver permissão (admin ou revisor) -->
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Editar</button>
-                </div>
-                <!-- distancia (IMPORTANTE) -->
-                <small class="text-muted">3.5km</small>
-              </div>
-            </div>
-          </div>
-        </div>
-
+      <div class="containerLinha">
+        <div class="linha"></div>
+          <div class="linhaIcon"><i class="material-icons">school</i></div>
+        <div class="linha"></div>
       </div>
 
-       <!-- TO-DO: melhorar esse botão e add função -->
-       <div class="centralizado">
-          <button type="button" label="Ver mais">Ver mais</button>
-        </div>
+      <h1>Cursos</h1>
+      <?php include_once __DIR__ . '/cursos.php'; ?>
+
+
       
     </div>
 </body>
