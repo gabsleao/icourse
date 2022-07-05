91c3e5f9de9bd04017bc6cc05ba62d16f5aad131
gabriel leão <gabidonn@gmail.com>
2022-07-05 16:42:20 -0300

    modal registrar

--- modal_registrar.php
+++ modal_registrar.php
@@ -3,51 +3,30 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalRegistro">Adicionar nova Instituição</h5>
+                <h5 class="modal-title" id="modalRegistro">Registrar</h5>
             </div>
             <div class="modal-body">
                 <div class="col-lg-12">
-                    <form onsubmit="novaInstituicao(this)">
+                    <form onsubmit="registrarUsuario(this)">
                         <div class="row g-3">
                             <div class="col-12">
-                                <label for="nome_instituicao" class="form-label">Nome</label><span style="color: red;"> *</span>
-                                <input type="text" class="form-control" id="nome_instituicao" placeholder="Unicamp..." required>
+                                <label for="nome_aluno" class="form-label">Nome</label><span style="color: red;"> *</span>
+                                <input type="text" class="form-control" id="nome_aluno" placeholder="Gabriel..." required>
                             </div>
 
                             <div class="col-12">
-                                <label for="descricao_instituicao" class="form-label">Descrição</label><span style="color: red;"> *</span>
-                                <textarea class="form-control" id="descricao_instituicao" placeholder="Universidade Estadual de Campinas considerada uma das melhores universidades do país e da América Latina..." required></textarea>
+                                <label for="email_aluno" class="form-label">E-mail</label><span style="color: red;"> *</span>
+                                <input type="text" class="form-control" id="email_aluno" placeholder="aluno@exemplo.com..." required>
                             </div>
 
                             <div class="col-12">
-                                <label for="tags_instituicao" class="form-label">Tags (separar for vírgula)</label><span style="color: red;"> *</span>
-                                <textarea class="form-control" id="tags_instituicao" placeholder="estadual,faculdade,publica,vestibular..." required></textarea>
+                                <label for="senha_aluno" class="form-label">Senha</label><span style="color: red;"> *</span>
+                                <input type="password" class="form-control" id="senha_aluno" placeholder="********" required>
                             </div>
 
                             <div class="col-12">
-                                <label for="endereco_instituicao" class="form-label">Endereço</label><span style="color: red;"> *</span>
-                                <input type="text" class="form-control" id="endereco_instituicao" placeholder="Cidade Universitária Zeferino Vaz, Barão Geraldo..." required>
-                            </div>
-
-                            <div class="col-md-4">
-                                <label for="cep" class="form-label">CEP</label>
-                                <input type="text" class="form-control" id="cep" placeholder="13083-970..." required>
-                            </div>
-
-                            <div class="col-md-4">
-                                <label for="estado_instituicao" class="form-label">Estado</label>
-                                <select class="form-select" id="estado_instituicao" required>
-                                    <option>Escolha...</option>
-                                    <option value="SP">São Paulo</option>
-                                </select>
-                            </div>
-
-                            <div class="col-md-4">
-                                <label for="cidade_instituicao" class="form-label">Cidade</label>
-                                <select class="form-select" id="cidade_instituicao" required>
-                                    <option>Escolha...</option>
-                                    <option value="CPS">Campinas</option>
-                                </select>
+                                <label for="senha_aluno_confirmacao" class="form-label">Confirmar Senha</label><span style="color: red;"> *</span>
+                                <input type="password" class="form-control" id="senha_aluno_confirmacao" placeholder="********" required>
                             </div>
                         </div>
                 </div>
@@ -60,3 +39,9 @@
         </div>
     </div>
 </div>
+
+<script>
+    $(document).ready(function(){
+        $('.modal-backdrop').remove('modal-backdrop');
+    });
+</script>
